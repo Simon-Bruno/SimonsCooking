96fa7d9ddb643051aa883460cf9d1abb957bbb3c
Simon <simonadrbruno@gmail.com>
2022-02-26 21:56:59 +0100

    Added Heroku SQL Support

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from functools import wraps
 import forms
 from forms import CreatePostForm
+import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -39,7 +40,7 @@ def load_user(user_id):
     return User.query.filter_by(id=user_id).first()
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
